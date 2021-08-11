ca04a3f85cf16cae93eef7aa749feb83cd67eeaa
Mohsen Nasseri <44729493+mnasserimn@users.noreply.github.com>
2021-08-11 10:54:13 +0430

    Update FSEB.py

--- FSEB.py
+++ FSEB.py
@@ -3,7 +3,7 @@
 # Developed by:  Mercedeh Taheri, Milad Shamsi Anboohi, Mohsen Nasseri, Majid Kiavarz, and Abdolmajid Mohammadian   
 # Email address: mrc.tahery@ut.ac.ir  
 # Created:       21/05/2020
-# Submitted to:  Agricutural and Forest Meterology, 
+# Submitted to:  Agricutural and Forest Meteorology, 
 
 
 #------------------------------------------------------------------------------
